206d9d449b5b3da06f8d0db445d20e8b88b3c1e8
Jose Zarabanda <josezarabanda@example.com>
2025-10-08 22:40:04 -0400

    feat: Initialize Appomodoro project structure with main app and content view

---
+++ IOS/GPT5_Codex/appomodoro/appomodoro/appomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  appomodoroApp.swift
+//  appomodoro
+//
+//  Created by JOSE ZARABANDA on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct appomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
