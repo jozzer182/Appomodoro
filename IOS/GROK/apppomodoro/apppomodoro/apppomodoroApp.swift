56d5870d729d905e2a6d6323df002295c7ee924a
Jose Zarabanda <josezarabanda@example.com>
2025-10-09 09:32:54 -0400

    Se incluyen pruebas de gemini y grok

---
+++ IOS/GROK/apppomodoro/apppomodoro/apppomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  apppomodoroApp.swift
+//  apppomodoro
+//
+//  Created by JOSE ZARABANDA on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct apppomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
